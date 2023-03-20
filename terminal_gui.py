6b6bbf041f81c9b42794a2f545d1658923df172f
IgorM <mivspb55@gmail.com>
2023-03-20 18:19:19 +0300

    add command history

--- terminal_gui.py
+++ terminal_gui.py
@@ -7,7 +7,7 @@ from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayo
 from PyQt6.QtWidgets import QPlainTextEdit, QLineEdit
 from PyQt6.QtCore import QEvent, Qt, QTimer
 
-step = 1.0
+step = 0.5
 
 IP = "192.168.1.100"    # IP for K-Roset
 PORT = 23         # Port for K-Roset
@@ -119,10 +119,34 @@ class KhiRoTerm:
         # print(incoming)
         return incoming
 
+    def read_r_variable(self, var_name):
+        command = "list /r " + var_name
+        self.server.sendall(command.encode())
+        self.server.sendall(footer_message)
+
+        kawasaki_msg = self.wait_recv([b'\x3e'], timeout=1)
+        self.parent.print_text(kawasaki_msg.decode("utf-8", 'ignore'))
+
+        tmp = kawasaki_msg.decode("utf-8", 'ignore').split('\r\n')
+        for element in tmp:
+            # print(element, element.find(var_name + ' ='))
+            if element.find(var_name + ' =') > -1:
+                return float(element.split(' ')[-1])
+
+    def write_r_variable(self, var_name, var_value):
+        command = var_name + " = " + str(var_value)
+        self.server.sendall(command.encode())
+        self.server.sendall(footer_message)
+
+        kawasaki_msg = self.wait_recv([b'\x3e'], timeout=1)
+        self.parent.print_text(kawasaki_msg.decode("utf-8", 'ignore'))
+
 
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__()
+        self.command_history = []
+        self.last_command_in_hist = 0
 
         self.white_compensation = 0
         self.green_compensation = 0
@@ -251,81 +275,139 @@ class MainWindow(QMainWindow):
 
         self.khiroterm = KhiRoTerm(IP, PORT, self)
 
+        self.read_all_variables()
+
 
     def button_white_up_function(self):
-        self.white_compensation = self.white_compensation + step
+        self.white_compensation = self.khiroterm.read_r_variable("white_comp") + step
+        self.khiroterm.write_r_variable("white_comp", self.white_compensation)
         self.label_white.setText(str(self.white_compensation))
 
     def button_white_down_function(self):
-        self.white_compensation = self.white_compensation - step
+        self.white_compensation = self.khiroterm.read_r_variable("white_comp") - step
+        self.khiroterm.write_r_variable("white_comp", self.white_compensation)
         self.label_white.setText(str(self.white_compensation))
 
     def button_green_up_function(self):
-        self.green_compensation = self.green_compensation + step
+        self.green_compensation = self.khiroterm.read_r_variable("green_comp") + step
+        self.khiroterm.write_r_variable("green_comp", self.green_compensation)
         self.label_green.setText(str(self.green_compensation))
 
     def button_green_down_function(self):
-        self.green_compensation = self.green_compensation - step
+        self.green_compensation = self.khiroterm.read_r_variable("green_comp") - step
+        self.khiroterm.write_r_variable("green_comp", self.green_compensation)
         self.label_green.setText(str(self.green_compensation))
 
     def button_red_up_function(self):
-        self.red_compensation = self.red_compensation + step
+        self.red_compensation = self.khiroterm.read_r_variable("red_comp") + step
+        self.khiroterm.write_r_variable("red_comp", self.red_compensation)
         self.label_red.setText(str(self.red_compensation))
 
     def button_red_down_function(self):
-        self.red_compensation = self.red_compensation - step
+        self.red_compensation = self.khiroterm.read_r_variable("red_comp") - step
+        self.khiroterm.write_r_variable("red_comp", self.red_compensation)
         self.label_red.setText(str(self.red_compensation))
 
     def button_blue_up_function(self):
-        self.blue_compensation = self.blue_compensation + step
+        self.blue_compensation = self.khiroterm.read_r_variable("blue_comp") + step
+        self.khiroterm.write_r_variable("blue_comp", self.blue_compensation)
         self.label_blue.setText(str(self.blue_compensation))
 
     def button_blue_down_function(self):
-        self.blue_compensation = self.blue_compensation - step
+        self.blue_compensation = self.khiroterm.read_r_variable("blue_comp") - step
+        self.khiroterm.write_r_variable("blue_comp", self.blue_compensation)
         self.label_blue.setText(str(self.blue_compensation))
 
     def button_black_up_function(self):
-        self.black_compensation = self.black_compensation + step
+        self.black_compensation = self.khiroterm.read_r_variable("black_comp") + step
+        self.khiroterm.write_r_variable("black_comp", self.black_compensation)
         self.label_black.setText(str(self.black_compensation))
 
     def button_black_down_function(self):
-        self.black_compensation = self.black_compensation - step
+        self.black_compensation = self.khiroterm.read_r_variable("black_comp") - step
+        self.khiroterm.write_r_variable("black_comp", self.black_compensation)
         self.label_black.setText(str(self.black_compensation))
 
     def button_yellow_up_function(self):
-        self.yellow_compensation = self.yellow_compensation + step
+        self.yellow_compensation = self.khiroterm.read_r_variable("yellow_comp") + step
+        self.khiroterm.write_r_variable("yellow_comp", self.yellow_compensation)
         self.label_yellow.setText(str(self.yellow_compensation))
 
     def button_yellow_down_function(self):
-        self.yellow_compensation = self.yellow_compensation - step
+        self.yellow_compensation = self.khiroterm.read_r_variable("yellow_comp") - step
+        self.khiroterm.write_r_variable("yellow_comp", self.yellow_compensation)
         self.label_yellow.setText(str(self.yellow_compensation))
 
     def button_canvas_up_function(self):
-        self.canvas_compensation = self.canvas_compensation + step
+        self.canvas_compensation = self.khiroterm.read_r_variable("canvas_comp") + step
+        self.khiroterm.write_r_variable("canvas_comp", self.canvas_compensation)
         self.label_canvas.setText(str(self.canvas_compensation))
 
     def button_canvas_down_function(self):
-        self.canvas_compensation = self.canvas_compensation - step
+        self.canvas_compensation = self.khiroterm.read_r_variable("canvas_comp") - step
+        self.khiroterm.write_r_variable("canvas_comp", self.canvas_compensation)
         self.label_canvas.setText(str(self.canvas_compensation))
 
     def button_water_up_function(self):
-        self.water_compensation = self.water_compensation + step
+        self.water_compensation = self.khiroterm.read_r_variable("water_comp") + step
+        self.khiroterm.write_r_variable("water_comp", self.water_compensation)
         self.label_water.setText(str(self.water_compensation))
 
     def button_water_down_function(self):
-        self.water_compensation = self.water_compensation - step
+        self.water_compensation = self.khiroterm.read_r_variable("water_comp") - step
+        self.khiroterm.write_r_variable("water_comp", self.water_compensation)
         self.label_water.setText(str(self.water_compensation))
 
     def print_text(self, text):
         self.text_terminal.insertPlainText(text)
         self.text_terminal.verticalScrollBar().setValue(self.text_terminal.verticalScrollBar().maximum())
 
+    def read_all_variables(self):
+        self.white_compensation = self.khiroterm.read_r_variable("white_comp")
+        self.green_compensation = self.khiroterm.read_r_variable("green_comp")
+        self.red_compensation = self.khiroterm.read_r_variable("red_comp")
+        self.blue_compensation = self.khiroterm.read_r_variable("blue_comp")
+        self.black_compensation = self.khiroterm.read_r_variable("black_comp")
+        self.yellow_compensation = self.khiroterm.read_r_variable("yellow_comp")
+        self.canvas_compensation = self.khiroterm.read_r_variable("canvas_comp")
+        self.water_compensation = self.khiroterm.read_r_variable("water_comp")
+
+        self.label_white.setText(str(self.white_compensation))
+        self.label_green.setText(str(self.green_compensation))
+        self.label_red.setText(str(self.red_compensation))
+        self.label_blue.setText(str(self.blue_compensation))
+        self.label_black.setText(str(self.black_compensation))
+        self.label_yellow.setText(str(self.yellow_compensation))
+        self.label_canvas.setText(str(self.canvas_compensation))
+        self.label_water.setText(str(self.water_compensation))
+
     def eventFilter(self, obj, event):
         if event.type() == QEvent.Type.KeyPress and obj is self.text_line:
-            if event.key() == 16777220 and self.text_line.hasFocus():
+            if event.key() == 16777220 and self.text_line.hasFocus():  # Enter
                 command = self.text_line.text()
                 self.text_line.clear()
+                if len(self.command_history) > 0:
+                    if self.command_history[-1] != command:
+                        self.command_history.append(command)
+                        self.last_command_in_hist = len(self.command_history)
                 self.khiroterm.send_command(command)
+
+            if event.key() == 16777235 and self.text_line.hasFocus():  # Up
+                if len(self.command_history) > 0:
+                    self.last_command_in_hist -= 1
+                    if self.last_command_in_hist < 0:
+                        self.last_command_in_hist = 0
+
+                    self.text_line.setText(self.command_history[self.last_command_in_hist])
+
+            if event.key() == 16777237 and self.text_line.hasFocus():  # Down
+                if len(self.command_history) > 0:
+                    self.last_command_in_hist += 1
+                    if self.last_command_in_hist > (len(self.command_history)-1):
+                        self.last_command_in_hist = (len(self.command_history)-1)
+
+                    self.text_line.setText(self.command_history[self.last_command_in_hist])
+
         return super().eventFilter(obj, event)
 
 
